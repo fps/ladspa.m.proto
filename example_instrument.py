c4d0506b00c16e838bd8eab79eaa5f2cb2813fbd
Florian Paul Schmidt <mista.tapas@gmx.net>
2013-04-02 00:11:36 +0200

    pimp the instrument a little

--- example_instrument.py
+++ example_instrument.py
@@ -7,6 +7,7 @@ def add_plugin(synth, library, label):
 	plugin = synth.plugins.add()
 	plugin.library = library
 	plugin.label = label
+	return plugin
 
 def make_connection(synth, source_index, source_port_index, sink_index, sink_port_index):
 	connection = synth.connections.add()
@@ -22,6 +23,11 @@ def make_voice_connection(instrument, source_index, source_port_index, sink_inde
 	connection.sink_index = sink_index
 	connection.sink_port_index = sink_port_index
 
+def set_port_value(plugin, port_index, the_value):
+	value = plugin.values.add()
+	value.port_index = port_index
+	value.value = the_value
+
 def expose_port(synth, plugin_index, port_index):
 	port = synth.exposed_ports.add()
 	port.plugin_index = plugin_index
@@ -36,7 +42,10 @@ synth = instrument.synth
 number_of_plugins = 0
 
 for n in range(5):
-	add_plugin(synth, '/usr/lib/ladspa/dahdsr_fexp.so', 'dahdsr_fexp')
+	plugin = add_plugin(synth, '/usr/lib/ladspa/dahdsr_fexp.so', 'dahdsr_fexp')
+	set_port_value(plugin, 6, 0)
+	set_port_value(plugin, 5, 0.2)
+	
 	
 	number_of_plugins += 1
 
@@ -57,6 +66,14 @@ for n in range(5):
 	make_voice_connection(instrument, n, 1, number_of_plugins - 3, 0)
 	make_voice_connection(instrument, n, 3, number_of_plugins - 2, 0)
 
+	plugin = add_plugin(synth,'/usr/lib/ladspa/delay.so', 'delay_5s')
+	set_port_value(plugin, 0, (1 + n) * 0.125)
+	set_port_value(plugin, 1, 0.25)
+
+	number_of_plugins += 1
+	
+	make_connection(synth, number_of_plugins - 2, 2, number_of_plugins - 1, 2)
+
 add_plugin(synth,'/usr/lib/ladspa/sum_1665.so', 'sum_iaia_oa')
 
 number_of_plugins += 1
@@ -64,7 +81,7 @@ number_of_plugins += 1
 expose_port(synth, number_of_plugins - 1, 2)
 
 for n in range(5):
-	make_connection(synth, 3 * n + 2, 2, number_of_plugins - 1, 0)
+	make_connection(synth, 4 * n + 3, 3, number_of_plugins - 1, 0)
 	pass
 
 f = open("/dev/stdout", "wb")

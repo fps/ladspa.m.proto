bc3d3a49065548fcd32cac62238288c1e885f8d4
Florian Paul Schmidt <mista.tapas@gmx.net>
2015-02-21 23:43:26 +0100

    remove some unneeded exposed ports

--- examples.py/kick001.py
+++ examples.py/kick001.py
@@ -12,7 +12,7 @@ voice_outs = []
 
 for voice in range(number_of_voices):
 	freq_env = add_plugin(synth, 'dahdsr_fexp')
-	set_port_value(synth, freq_env, 5, 0.05)
+	set_port_value(synth, freq_env, 5, 1)
 	set_port_value(synth, freq_env, 6, 0)
 	set_port_value(synth, freq_env, 7, 0.01)
 
@@ -30,14 +30,14 @@ for voice in range(number_of_voices):
 	make_voice_connection(instrument, voice, FREQUENCY, freq_env_sum, 1)
 
 
-	osc = add_plugin(synth, 'sawtooth_fa_oa')
+	osc = add_plugin(synth, 'ladspam-sine-osc')
 	## set_port_value(synth, osc, 1, 1)
 	
 	make_connection(synth, freq_env_sum, 2, osc, 0)
 	
 	
 	amp_env = add_plugin(synth, 'dahdsr_fexp')
-	set_port_value(synth, amp_env, 5, 0.1)
+	set_port_value(synth, amp_env, 5, 1)
 	set_port_value(synth, amp_env, 6, 0.0)
 	set_port_value(synth, amp_env, 7, 0.01)
 
@@ -49,7 +49,7 @@ for voice in range(number_of_voices):
 
 	make_connection(synth, amp_env, 8, amp_env_prod, 0)
 	
-	make_connection(synth, osc, 1, amp_env_prod, 1)
+	make_connection(synth, osc, 4, amp_env_prod, 1)
 	
 	voice_outs.append(amp_env_prod)
 	
@@ -59,6 +59,6 @@ for voice in range(number_of_voices):
 	make_connection(synth, voice_outs[voice], 2, sum, 0)
 
 expose_port(synth, sum, 2)
-expose_port(synth, sum, 2)
+# expose_port(synth, sum, 2)
 
 dump_instrument(instrument)
